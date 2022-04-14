0433e3648fbd36b1a093b5981e6655fe414340aa
Maithri Sharma <maithribharadwaj3@gmail.com>
2022-04-14 15:09:59 +0100

    Added images and related changes

--- Task1/recipe.js
+++ Task1/recipe.js
@@ -7,7 +7,7 @@ function makeAjaxRequest(){
         success: function(response){
             let txt="";
             $.each(response.recipes,function(index){
-                txt+="<tr><td>"+"#"+"</td><td>"+response.recipes[index].name
+                txt+="<tr><td><img src='"+response.recipes[index].image+"' style='width:200px;height:200px'/></td><td>"+response.recipes[index].name
                 +"</td><td>"+response.recipes[index].prep+"</td><td>"+response.recipes[index].ingredients+"</td><td>"+response.recipes[index].method+"</td></tr>";
             });
             $("#recipelist").append(txt);
@@ -17,7 +17,3 @@ function makeAjaxRequest(){
         }
     })
 }
-$(window).on("load resize ", function() {
-    var scrollWidth = $('.tbl-content').width() - $('.tbl-content table').width();
-    $('.tbl-header').css({'padding-right':scrollWidth});
-  }).resize();
