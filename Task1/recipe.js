17693d2b5f2981deb92aa00edbce8fcac9acc738
Maithri Sharma <maithribharadwaj3@gmail.com>
2022-04-19 15:55:58 +0100

    adding modal for ingredients

--- Task1/recipe.js
+++ Task1/recipe.js
@@ -9,7 +9,7 @@
             let txt="";
             $.each(response.recipes,function(index){
                 txt+="<tr><td><img src='"+response.recipes[index].image+"' style='width:200px;height:200px'/></td><td>"+response.recipes[index].name
-                +"</td><td>"+response.recipes[index].prep+"</td><td>"+response.recipes[index].ingredients+"</td><td>"+response.recipes[index].method+"</td></tr>";
+                +"</td><td>"+response.recipes[index].prep+"</td><td><button id='"+response.recipes[index].id+"'onclick='Ingredients("+response.recipes[index].id+")'>Ingredients</button>"+response.recipes[index].ingredients+"</td><td>"+response.recipes[index].method+"</td></tr>";
             });
             $("#recipelist").append(txt);
         },
@@ -19,3 +19,30 @@
     });
     }, 250);
 }());
+
+function Ingredients(Id){
+    var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+}
