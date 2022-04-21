435dc2de64c02ab0ff5de2a6674ded09f9faf2d6
Maithri Sharma <maithribharadwaj3@gmail.com>
2022-04-21 19:07:47 +0100

    nutritions modal

--- Task1/recipe.js
+++ Task1/recipe.js
@@ -9,15 +9,17 @@
             let txt="";
             $('#recipelist').empty();
             $.each(response.recipes,function(index){
+                var nutritions=JSON.stringify(response.recipes[index].nutrition);
                 var ingredients=JSON.stringify(response.recipes[index].ingredients);
                 //console.log(ingredients);
                 //console.log(JSON.parse(ingredients));
                 var methods=JSON.stringify(response.recipes[index].method);
                 txt+="<tr><td style='width:300px;'><img src='"+response.recipes[index].image+
                 "' class='img'/></td><td><h1 class='name'>"+response.recipes[index].name
-                +"</h1><p class='info'>By: "+response.recipes[index].author+"&emsp;<span>Ratings: "+response.recipes[index].ratings+"</span><br><br>"+response.recipes[index].desc+"<br><br>Prep Time: "+response.recipes[index].prep+"<br>Cook Time: "+response.recipes[index].cook+"</p><button class='btn' id='"+"Ingredient"+
+                +"</h1><p class='info'>By: "+response.recipes[index].author+"&emsp;<span>Ratings: "+response.recipes[index].ratings+"</span><br><br>"+response.recipes[index].desc+"<br><br>Prep Time: "+response.recipes[index].prep+"<br>Cook Time: "+response.recipes[index].cook+"</p><button class='btn' onclick='Nutrition("+nutritions+
+                ")'>Nutritions</button><span>&emsp;<button class='btn' id='"+"Ingredient"+
                 response.recipes[index].id+"'onclick='Ingredients("+ingredients+
-                ")'>Ingredients</button>&emsp;<span><button onclick='Method("+methods+")'>Method</button></span></td></tr>";
+                ")'>Ingredients</button>&emsp;<button onclick='Method("+methods+")'>Method</button></span></td></tr>";
             });
             $("#recipelist").append(txt);
             //makeAjaxRequest();
@@ -28,6 +30,40 @@
     });
     }, 2500);
 })();
+
+function Nutrition(list){
+  console.log(list);
+  var modal = document.getElementById("nutritionModal");
+  var data = document.getElementById("nutritionData");  
+  var nutritionhtml="<ul>"
+  //list.forEach(element => {
+      //ingredientshtml+="<li>"+element+"</li><br>";
+  //});
+  for (const property in list) {
+    nutritionhtml+="<li>"+(`${property}: ${list[property]}`)+"</li><br>";
+  }
+  nutritionhtml+="</ul>";
+  data.innerHTML=nutritionhtml;
+  modal.style.display = "block";
+
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+if (event.target == modal) {
+  modal.style.display = "none";
+}
+}
+
+}
 
 function Ingredients(list){
     console.log(list);
@@ -43,7 +79,7 @@ function Ingredients(list){
 
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+var span = document.getElementsByClassName("close")[1];
 
 
 // When the user clicks on <span> (x), close the modal
@@ -75,7 +111,7 @@ function Method(list){
 
 
 // Get the <span> element that closes the modal
-var span= document.getElementsByClassName("close")[1];
+var span= document.getElementsByClassName("close")[2];
 console.log(span);
 
 
