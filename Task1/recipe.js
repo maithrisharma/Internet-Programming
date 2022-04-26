72096648832d050e6f839d8e0cb6fd0b16174662
Maithri Sharma <maithribharadwaj3@gmail.com>
2022-04-26 10:48:35 +0100

    Ajax using JS

--- Task1/recipe.js
+++ Task1/recipe.js
@@ -1,35 +1,90 @@
 
-(function makeAjaxRequest(){
-    setTimeout(function(){
-    $.ajax( {
-        type:'GET',
-        url:'recipe.json',
-        dataType:"json",
-        success: function(response){
-            let txt="";
-            $('#recipelist').empty();
-            $.each(response.recipes,function(index){
-                var nutritions=JSON.stringify(response.recipes[index].nutrition);
-                var ingredients=JSON.stringify(response.recipes[index].ingredients);
-                //console.log(ingredients);
-                //console.log(JSON.parse(ingredients));
-                var methods=JSON.stringify(response.recipes[index].method);
-                txt+="<tr><td style='width:300px;'><a href='#'><img src='"+response.recipes[index].image+
-                "' class='img'/></a></td><td><h1 class='name'>"+response.recipes[index].name
-                +"</h1><p class='info'>By: "+response.recipes[index].author+"&emsp;<span>Ratings: "+response.recipes[index].ratings+"</span><br><br>"+response.recipes[index].desc+"<br><br>Prep Time: "+response.recipes[index].prep+"<br>Cook Time: "+response.recipes[index].cook+"</p><button class='btn' onclick='Nutrition("+nutritions+
-                ")'>Nutritions</button><span>&emsp;<button class='btn' id='"+"Ingredient"+
-                response.recipes[index].id+"'onclick='Ingredients("+ingredients+
-                ")'>Ingredients</button>&emsp;<button onclick='Method("+methods+")'>Method</button></span></td></tr>";
-            });
-            $("#recipelist").append(txt);
-            //makeAjaxRequest();
-        },
-        error: function(){
-            $("#updatemessage").append("Error");
-        }
-    });
-    }, 2500);
+(function Recursive(){
+  AjaxRequest();
+  setTimeout(Recursive,2500);
+  
 })();
+function AjaxRequest(){
+  if(window.XMLHttpRequest){
+    xhr = new XMLHttpRequest();
+  }
+  else{
+    if(window.ActiveXObject){
+      xhr = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+  }
+  if(xhr){
+    xhr.open("GET","recipe.json",true);
+    xhr.send();
+    xhr.onreadystatechange= showContents;
+  }
+  else{
+    document.getElementById("updatemessage").innerHTML="Error";
+  }
+}
+
+function showContents(){
+  if(xhr.readyState == 4){
+    if(xhr.status==200){
+      let response= JSON.parse(xhr.responseText);
+      let txt ="";
+      for(let i in response.recipes){
+        var nutritions=JSON.stringify(response.recipes[i].nutrition);
+            var ingredients=JSON.stringify(response.recipes[i].ingredients);
+            //console.log(ingredients);
+            //console.log(JSON.parse(ingredients));
+            var methods=JSON.stringify(response.recipes[i].method);
+            txt+="<tr><td style='width:300px;'><a href='#'><img src='"+response.recipes[i].image+
+            "' class='img'/></a></td><td><h1 class='name'>"+response.recipes[i].name
+            +"</h1><p class='info'>By: "+response.recipes[i].author+"&emsp;<span>Ratings: "+response.recipes[i].ratings+"</span><br><br>"+response.recipes[i].desc+"<br><br>Prep Time: "+response.recipes[i].prep+"<br>Cook Time: "+response.recipes[i].cook+"</p><button class='btn' onclick='Nutrition("+nutritions+
+            ")'>Nutritions</button><span>&emsp;<button class='btn' id='"+"Ingredient"+
+            response.recipes[i].id+"'onclick='Ingredients("+ingredients+
+            ")'>Ingredients</button>&emsp;<button onclick='Method("+methods+")'>Method</button></span></td></tr>";
+      }
+      console.log(txt);
+      document.getElementById("recipelist").innerHTML=txt;
+    }
+    else{
+      document.getElementById("updatemessage").innerHTML="Error";
+    }
+  }
+}
+
+
+
+
+/*(function makeAjaxRequest(){
+  $.ajax( {
+    type:'GET',
+    url:'recipe.json',
+    dataType:"json",
+    success: function(response){
+        let txt="";
+        //$('#recipelist').empty();
+        $.each(response.recipes,function(index){
+            var nutritions=JSON.stringify(response.recipes[index].nutrition);
+            var ingredients=JSON.stringify(response.recipes[index].ingredients);
+            //console.log(ingredients);
+            //console.log(JSON.parse(ingredients));
+            var methods=JSON.stringify(response.recipes[index].method);
+            txt+="<tr><td style='width:300px;'><a href='#'><img src='"+response.recipes[index].image+
+            "' class='img'/></a></td><td><h1 class='name'>"+response.recipes[index].name
+            +"</h1><p class='info'>By: "+response.recipes[index].author+"&emsp;<span>Ratings: "+response.recipes[index].ratings+"</span><br><br>"+response.recipes[index].desc+"<br><br>Prep Time: "+response.recipes[index].prep+"<br>Cook Time: "+response.recipes[index].cook+"</p><button class='btn' onclick='Nutrition("+nutritions+
+            ")'>Nutritions</button><span>&emsp;<button class='btn' id='"+"Ingredient"+
+            response.recipes[index].id+"'onclick='Ingredients("+ingredients+
+            ")'>Ingredients</button>&emsp;<button onclick='Method("+methods+")'>Method</button></span></td></tr>";
+        });
+       // $("#recipelist").append(txt);
+       document.getElementById("recipeList").innerHTML=txt;
+        setTimeout(makeAjaxRequest,2500);
+    },
+    error: function(){
+        $("#updatemessage").append("Error");
+    }
+
+});
+    
+})();*/
 
 function Nutrition(list){
   console.log(list);
