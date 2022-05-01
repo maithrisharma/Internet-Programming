ef3f723c676c0c54699af9efaedc5bff09810ce1
Maithri Sharma <maithribharadwaj3@gmail.com>
2022-05-01 19:07:46 +0100

    Removed jquery from comments

--- Task1/recipe.js
+++ Task1/recipe.js
@@ -55,41 +55,6 @@ function showContents(){
 }
 
 
-
-
-/*(function makeAjaxRequest(){
-  $.ajax( {
-    type:'GET',
-    url:'recipe.json',
-    dataType:"json",
-    success: function(response){
-        let txt="";
-        //$('#recipelist').empty();
-        $.each(response.recipes,function(index){
-            var nutritions=JSON.stringify(response.recipes[index].nutrition);
-            var ingredients=JSON.stringify(response.recipes[index].ingredients);
-            //console.log(ingredients);
-            //console.log(JSON.parse(ingredients));
-            var methods=JSON.stringify(response.recipes[index].method);
-            txt+="<tr><td style='width:300px;'><a href='#'><img src='"+response.recipes[index].image+
-            "' class='img'/></a></td><td><h1 class='name'>"+response.recipes[index].name
-            +"</h1><p class='info'>By: "+response.recipes[index].author+"&emsp;<span>Ratings: "+response.recipes[index].ratings+"</span><br><br>"+response.recipes[index].desc+"<br><br>Prep Time: "+response.recipes[index].prep+"<br>Cook Time: "+response.recipes[index].cook+"</p><button class='btn' onclick='Nutrition("+nutritions+
-            ")'>Nutritions</button><span>&emsp;<button class='btn' id='"+"Ingredient"+
-            response.recipes[index].id+"'onclick='Ingredients("+ingredients+
-            ")'>Ingredients</button>&emsp;<button onclick='Method("+methods+")'>Method</button></span></td></tr>";
-        });
-       // $("#recipelist").append(txt);
-       document.getElementById("recipeList").innerHTML=txt;
-        setTimeout(makeAjaxRequest,2500);
-    },
-    error: function(){
-        $("#updatemessage").append("Error");
-    }
-
-});
-    
-})();*/
-
 function Nutrition(list){
   console.log(list);
   var modal = document.getElementById("nutritionModal");
