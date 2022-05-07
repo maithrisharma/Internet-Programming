f56667dcaf99580c44bd5e073dca02deaee82a2f
Maithri Sharma <maithribharadwaj3@gmail.com>
2022-05-07 10:18:45 +0100

    script moved to js

--- Task2/src/SampleRecipeReport.php
+++ Task2/src/SampleRecipeReport.php
@@ -27,6 +27,7 @@
        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
        <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.js"></script>
+       <script src="script.js"></script>
      </head>
      <body>
      <header>
@@ -110,44 +111,7 @@ $recipeID = $_GET['recipe'];
        <canvas id="myChart" width="400" height="400"></canvas> 
        </div>
        <script>
-           const ctx = document.getElementById('myChart').getContext('2d');
-    console.log(ctx);
-    const myChart = new Chart(ctx, {
-    type: 'pie',
-    data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(0, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(0, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        // resnponsive:true;
-        // maintainAspectRatio: false;
-        scales: {
-            y: {
-                beginAtZero: true
-            }
-        }
-    }
-    });
+
        </script>
 <?php       
 }
