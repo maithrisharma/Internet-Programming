4dc5c76d6a7f938a61c003ff899b188dcee9dc80
Maithri Sharma <maithribharadwaj3@gmail.com>
2022-05-09 22:54:46 +0100

    removed commented code

--- Task2/src/connection.php
+++ Task2/src/connection.php
@@ -1,18 +1,5 @@
 <?php
-// $server = 'db';
-// $username = 'root';
-// $password = 'csym019';
-// //The name of the schema/database we created earlier in Adminer
-// //If this schema/database does not exist you will get an error!
-// $schema = 'Login';
-// $pdo = new PDO('mysql:dbname=' . $schema . ';host=' . $server, $username, $password,
-// [ PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
-// $stmt = $pdo->prepare('SELECT * FROM Recipes');
 
-// $stmt->execute();
-// foreach ($stmt as $row) {
-//  echo '<p>' . $row['name'] . '</p>';
-// }
 
 $db_host="db"; //localhost server 
 $db_user="root"; //database username
